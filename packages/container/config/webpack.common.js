a9824480375e3e320fee0ce3c7ac48a42141299a
rakhizende18 <rakhizende1809@gmail.com>
2024-01-09 12:40:29 +0100

    mfe

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,7 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     module: {
         rules: [
-           { test: /\.m?js$/,
+           { 
+            test: /\.m?js$/,
             exclude: /node_modules/,
             use: {
                 loader: 'babel-loader',
@@ -11,5 +14,10 @@ module.exports = {
                 }
             }}
         ]
-    }
+    },
+    plugins: [ 
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
 }
